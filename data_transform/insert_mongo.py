59d8d787a65641fb43b926154660113b643ca257
Matthew Favetti <mpfavetti@ucdavis.edu>
2021-11-10 11:11:39 -0800

    Adding code to transform woodland shapefile to geojson for map demo

--- data_transform/insert_mongo.py
+++ data_transform/insert_mongo.py
@@ -6,6 +6,6 @@ client = MongoClient("mongodb://bctool:phev@localhost:27017")
 dbname = client['bctool']
 collection_name = dbname['features']
 
-geojson = json.load(open(os.path.join('input', 'yolo_osm_id.geojson')))
+geojson = json.load(open(os.path.join('output', 'woodland.json')))
 
 collection_name.insert_many(geojson['features'])
---
+++ data_transform/shp_to_json.py
@@ -0,0 +1,11 @@
+import shapefile
+import json
+import os
+
+infilename = os.path.join('input', 'neighborhood_ways', 'neighborhood_ways')
+
+sf = shapefile.Reader(infilename)
+records = sf.shapeRecords()
+geojson = records.__geo_interface__
+
+json.dump(geojson, open(os.path.join('output', 'woodland.json'), 'w'))
