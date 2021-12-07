47d48590bce3d3ece2b01838747078d8dcf031ea
Matthew Favetti <mpfavetti@ucdavis.edu>
2021-12-07 15:56:22 -0800

    Updating to run sacramento as well and to filter properties

--- data_transform/shp_to_json.py
+++ data_transform/shp_to_json.py
@@ -2,10 +2,44 @@ import shapefile
 import json
 import os
 
-infilename = os.path.join('input', 'neighborhood_ways', 'neighborhood_ways_latlong')
+needed_fields = [
+    "TDG_ID",
+    "ONE_WAY_CA",
+    "INTERSECTI",
+    "INTERSE_01",
+]
 
-sf = shapefile.Reader(infilename)
-records = sf.shapeRecords()
-geojson = records.__geo_interface__
+files = [
+    {
+        "folder": "neighborhood_ways_woodland",
+        "shapefile": "neighborhood_ways_latlong",
+        "outfilename": "woodland.json",
+    },
+    # {
+    #     "folder": "neighborhood_ways_sacramento",
+    #     "shapefile": "neighborhood_ways",
+    #     "outfilename": "sacramento.json",
+    # },
+]
 
-json.dump(geojson, open(os.path.join('output', 'woodland.json'), 'w'))
+for file in files:
+
+    infilename = os.path.join('input', file['folder'], file['shapefile'])
+
+    sf = shapefile.Reader(infilename)
+    records = sf.shapeRecords()
+    geojson = records.__geo_interface__
+
+    for f in geojson['features']:
+
+        new_props = {}
+
+        for p in f['properties']:
+
+            if p in needed_fields:
+                new_props[p] = f['properties'][p]
+
+        f['properties'] = new_props
+
+    json.dump(geojson, open(os.path.join('output', file['outfilename']), 'w'))
+
