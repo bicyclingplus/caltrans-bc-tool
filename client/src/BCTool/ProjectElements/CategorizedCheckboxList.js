3eca5d2f3834f1cc0cefcc1018f9ca24b54d0de5
Matthew Favetti <mpfavetti@ucdavis.edu>
2022-02-04 18:28:01 -0800

    Adding tooltip for multi element interventions

--- client/src/BCTool/ProjectElements/CategorizedCheckboxList.js
+++ client/src/BCTool/ProjectElements/CategorizedCheckboxList.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import CheckboxList from './CheckboxList';
 
+const Tooltip = require('bootstrap/js/dist/tooltip');
+
 class CategorizedCheckboxList extends React.Component {
+
+    componentDidMount() {
+        const { category } = this.props;
+        if(category.tooltip) {
+            this.tooltip = new Tooltip(document.getElementById(`${category.shortname}-tooltip`));
+        }
+    }
+
+    componentWillUnmount() {
+        const { category } = this.props;
+        if(category.tooltip) {
+            this.tooltip.dispose();
+        }
+    }
 
     onChange = (shortname, value) => {
 
@@ -18,7 +35,17 @@ class CategorizedCheckboxList extends React.Component {
         return (
             <>
             <div className="mb-3">
-                <h6 className="dropdown-header">{category.label}</h6>
+                <h6 className="dropdown-header">
+                    {category.label}
+                    { category.tooltip ?
+                    <i id={`${category.shortname}-tooltip`}
+                        className="bi bi-info-circle ms-2"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="right"
+                        data-bs-html="true"
+                        title={category.tooltip}></i>
+                    : null }
+                </h6>
                 <CheckboxList
                     items={category.items}
                     onChange={this.onChange}
