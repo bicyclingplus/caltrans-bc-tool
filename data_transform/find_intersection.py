81b796e20d76e770211d6f7beaeb9bda40d60a2c
Matthew Favetti <mpfavetti@ucdavis.edu>
2022-02-04 16:53:22 -0800

    New network from Sravya

--- data_transform/find_intersection.py
+++ data_transform/find_intersection.py
@@ -1,16 +1,16 @@
 import json
 import os
 
-data = json.load(open(os.path.join('input', 'Sacramento_Bike_Network.geojson')))
+data = json.load(open(os.path.join('input', 'Sacramento_Bike_Predictions_check1.geojson')))
 
 for f in data['features']:
-    if f['properties']['edgeUID'] == 212462008:
+    if f['properties']['edgeUID'] == 212467000:
         print(f)
         break
 
 data = json.load(open(os.path.join('input', 'Sacramento_Ped_Network.geojson')))
 
 for f in data['features']:
-    if f['properties']['nodeID'] == 33612:
+    if f['properties']['nodeID'] == 48216:
         print(f)
         break
--- data_transform/new_network.py
+++ data_transform/new_network.py
@@ -5,17 +5,22 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://bctool:phev@localhost:27017")
 dbname = client['bctool']
 
+print('Starting ways')
+
 collection_name = dbname['ways']
 collection_name.delete_many({})
 
 way_files = [
-    "Sacramento_Bike_Network.geojson",
+    # "Sacramento_Bike_Network.geojson",
+    "Sacramento_Bike_Predictions_check1.geojson",
 ]
 
 for file in way_files:
     geojson = json.load(open(os.path.join('input', file)))
     collection_name.insert_many(geojson['features'])
 
+
+print('Starting intersections')
 
 collection_name = dbname['intersections']
 collection_name.delete_many({})
