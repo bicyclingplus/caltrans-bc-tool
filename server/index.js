ba9f02b937f590f234994760f391a8e656ec9296
Matthew Favetti <mpfavetti@ucdavis.edu>
2022-03-31 16:24:56 -0700

    Adding endpoint input validation

--- server/index.js
+++ server/index.js
@@ -38,56 +38,95 @@ tool.get("/api/geojson/:project", (req, res) => {
   }
 });
 
+
+// this endpoint takes in the corners of a latlng bounding box
+// the corners of the bounding box are then transformed into a geojson polygon
+// and used to query for ways/intersections that intersect the polygon
+// we need all four url paramters and the two latlngs must be different
+//
+// y1, x1 is the latlng for the south west corner of the bounding box
+// y2, x2 is the latlng for north east corner of the bounding box
+//
+// y1 = South latitude
+// x1 = West longitude
+//
+// y2 = North latitude
+// x2 = East longitude
+//
+// The polygon is then defined with 5 points A, B, C, D, A
+// A (x1, y1) South West corner
+// B (x1, y2) North West corner
+// C (x2, y2) North East corner
+// D (y2, y1) South East corner
+// A (x1, y1) Back to the SW corner to close the polygon
 tool.get("/api/bounds", async (req, res) => {
+  if(!req.query.x1 || !req.query.x2 || !req.query.y1 || !req.query.y2) {
+    return res.status(400).send({ "error": "All four bounding box coordinates (x1, y1, x2, and y2) are required."});
+  }
 
-    let query = {
-    };
+  let x1 = parseFloat(req.query.x1);
+  let x2 = parseFloat(req.query.x2);
+  let y1 = parseFloat(req.query.y1);
+  let y2 = parseFloat(req.query.y2);
 
-    // filter by map bounds if needed
-    if(req.query.x1 && req.query.x2 && req.query.y1 && req.query.y2) {
+  if(isNaN(x1) || x1 > 180 || x1 < -180) {
+    return res.status(400).send({ "error": "Parameter x1 is invalid longitude"});
+  }
 
-      let x1 = parseFloat(req.query.x1);
-      let x2 = parseFloat(req.query.x2);
-      let y1 = parseFloat(req.query.y1);
-      let y2 = parseFloat(req.query.y2);
+  if(isNaN(x2) || x2 > 180 || x2 < -180) {
+    return res.status(400).send({ "error": "Parameter x2 is invalid longitude"});
+  }
 
-      query.geometry = {
-        "$geoIntersects": {
-          "$geometry": {
-            "type": "Polygon",
-            "coordinates": [[
-              [x1, y1],
-              [x1, y2],
-              [x2, y2],
-              [x2, y1],
-              [x1, y1],
-            ]]
-          },
+  if(isNaN(y1) || y1 > 90 || y1 < -90) {
+    return res.status(400).send({ "error": "Parameter y1 is invalid latitude"});
+  }
+
+  if(isNaN(y2) || y2 > 90 || y2 < -90) {
+    return res.status(400).send({ "error": "Parameter y2 is invalid latitude"});
+  }
+
+  if(x1 === x2 && y1 === y2) {
+    return res.status(400).send({ "error": "SW and NE Bounding box corners must be different."});
+  }
+
+  let query = {
+    "geometry": {
+      "$geoIntersects": {
+        "$geometry": {
+          "type": "Polygon",
+          "coordinates": [[
+            [x1, y1],
+            [x1, y2],
+            [x2, y2],
+            [x2, y1],
+            [x1, y1],
+          ]]
         },
-      };
-    }
-
-    // Connect the client to the server
-    await client.connect();
-
-    const database = client.db('bctool');
-
-    let collection = database.collection('ways');
-    const ways = await collection.find(query).toArray();
-
-    collection = database.collection('intersections');
-    const intersections = await collection.find(query).toArray();
-
-    res.json({
-      "ways": {
-        "type": "FeatureCollection",
-        "features": ways,
       },
-      "intersections": {
-        "type": "FeatureCollection",
-        "features": intersections,
-      }
-    });
+    }
+  };
+
+  // Connect the client to the server
+  await client.connect();
+
+  const database = client.db('bctool');
+
+  let collection = database.collection('ways');
+  const ways = await collection.find(query).toArray();
+
+  collection = database.collection('intersections');
+  const intersections = await collection.find(query).toArray();
+
+  res.json({
+    "ways": {
+      "type": "FeatureCollection",
+      "features": ways,
+    },
+    "intersections": {
+      "type": "FeatureCollection",
+      "features": intersections,
+    }
+  });
 });
 
 app.use('/caltrans-bc-tool', tool);
