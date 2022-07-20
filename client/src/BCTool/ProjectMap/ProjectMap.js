e4d505fc8a6adff6a4c9c14048a6cb803642b72c
Matthew Favetti <mpfavetti@ucdavis.edu>
2022-07-20 14:35:50 -0700

    Increasing max zoom

--- client/src/BCTool/ProjectMap/ProjectMap.js
+++ client/src/BCTool/ProjectMap/ProjectMap.js
@@ -600,7 +600,7 @@ class ProjectMap extends React.Component {
 
         Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 19,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
