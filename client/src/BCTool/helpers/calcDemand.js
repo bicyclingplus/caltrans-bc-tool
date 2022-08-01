18ac27ea2f1c4014ddf4499944bff11e2b55ef68
Matthew Favetti <mpfavetti@ucdavis.edu>
2022-07-31 17:17:08 -0700

    Updating demand calculation for bikes to use a similar algorithm to pedestrians

--- client/src/BCTool/helpers/calcDemand.js
+++ client/src/BCTool/helpers/calcDemand.js
@@ -2,32 +2,33 @@
 // and ported to JS by me
 // JSON file required below also taken from the same repo
 // Input of 1.8, 10, 1234 should yield output of 258.42, verified
-const pedestrianConfig = require('../data/pedestrian_config.json');
+const config = require('../data/volume_to_miles.json');
 
-const weightPedestrianDemand = (proj_distance, proj_intersections, proj_volume) => {
-
-	// Get data from the lookup table in config.json
-	let miles_distribution = pedestrianConfig.miles_walked;
+const _weightDemand = (
+  proj_distance,
+  proj_units,
+  proj_volume,
+  miles_distribution) => {
 
 	// Calculating the average distance per intersection for the project
-	let proj_distance_per_intersection = proj_distance/proj_intersections;
+	let proj_distance_per_unit = proj_distance/proj_units;
 
 	// Convert miles into intersections
-    let intersection_distribution = {};
+    let unit_distribution = {};
     let distribution_den = 0;
 
     for(let dist in miles_distribution) {
 
-    	intersection_distribution[dist] = Math.floor(parseFloat(dist)/proj_distance_per_intersection);
+    	unit_distribution[dist] = Math.floor(parseFloat(dist)/proj_distance_per_unit);
 
     	// If on average people walk more than the number of intersections in the project,
     	// then consider they have walked through all of the project intersections
-    	if(intersection_distribution[dist] > proj_intersections) {
-    		intersection_distribution[dist] = proj_intersections;
+    	if(unit_distribution[dist] > proj_units) {
+    		unit_distribution[dist] = proj_units;
     	}
 
     	// Distribution of people walking through intersections
-    	distribution_den += intersection_distribution[dist]*miles_distribution[dist];
+    	distribution_den += unit_distribution[dist]*miles_distribution[dist];
     }
 
     let people = Math.floor(proj_volume/distribution_den);
@@ -35,7 +36,7 @@ const weightPedestrianDemand = (proj_distance, proj_intersections, proj_volume) 
     // Calculating the distance walked in the project
     let distance = 0;
 
-    for(let dist in  miles_distribution) {
+    for(let dist in miles_distribution) {
     	if(parseFloat(dist)>proj_distance) {
     		distance += proj_distance*miles_distribution[dist]*people;
     	}
@@ -49,26 +50,22 @@ const weightPedestrianDemand = (proj_distance, proj_intersections, proj_volume) 
     return Math.round(distance + Number.EPSILON);
 };
 
-const calcPedDemand = (existingTravel, selectedIntersections, userIntersections, projectLength) => {
+const _calcPedDemand = (
+  existingTravel,
+  selectedIntersections,
+  userIntersections,
+  projectLength) => {
 
-	// Grab the averages
+    // Grab the averages
     // Avg lower/mean/upper/pops/jobs used for user defined intersections
-    // let intersectionLower = [];
     let intersectionAvg = [];
-    // let intersectionUpper = [];
     let intersectionPops = [];
     let intersectionJobs = [];
 
     for(let intersection of selectedIntersections) {
-      // if(intersection.properties.low_pred) {
-      //   intersectionLower.push(parseInt(intersection.properties.low_pred));
-      // }
       if(intersection.properties.ped_demand) {
         intersectionAvg.push(parseInt(intersection.properties.ped_demand));
       }
-      // if(intersection.properties.high_pred) {
-      //   intersectionUpper.push(parseInt(intersection.properties.high_pred));
-      // }
       if(intersection.properties.population) {
         intersectionPops.push(parseInt(intersection.properties.population));
       }
@@ -77,9 +74,7 @@ const calcPedDemand = (existingTravel, selectedIntersections, userIntersections,
       }
     }
 
-    // let avgIntersectionLower = intersectionLower.length ? intersectionLower.reduce((a,b) => a+b) / intersectionLower.length : null;
     let avgIntersectionAvg = intersectionAvg.length ? intersectionAvg.reduce((a,b) => a+b) / intersectionAvg.length : null;
-    // let avgIntersectionUpper = intersectionUpper.length ? intersectionUpper.reduce((a,b) => a+b) / intersectionUpper.length : null;
     let avgIntersectionPops = intersectionPops.length ? intersectionPops.reduce((a,b) => a+b) / intersectionPops.length : null;
     let avgIntersectionJobs = intersectionJobs.length ? intersectionJobs.reduce((a,b) => a+b) / intersectionJobs.length : null;
 
@@ -89,41 +84,22 @@ const calcPedDemand = (existingTravel, selectedIntersections, userIntersections,
     // we add these to the total here
     for(let intersection of selectedIntersections) {
 
-      // let lower = parseInt(intersection.properties.low_pred);
       let mean = parseInt(intersection.properties.ped_demand);
-      // let upper = parseInt(intersection.properties.high_pred);
       let population = intersection.properties.population;
       let jobs = intersection.properties.jobs;
 
-      // existingTravel.miles.pedestrian.lower += lower;
       existingTravel.miles.pedestrian.mean += mean;
-      // existingTravel.miles.pedestrian.upper += upper;
-
-      // existingTravel.capita.pedestrian.lower += lower / population;
       existingTravel.capita.pedestrian.mean += mean / population;
-      // existingTravel.capita.pedestrian.upper += upper / population;
-
-      // existingTravel.jobs.pedestrian.lower += lower / jobs;
       existingTravel.jobs.pedestrian.mean += mean / jobs;
-      // existingTravel.jobs.pedestrian.upper += upper / jobs;
-
     }
 
     // CALCULATE PEDESTRIAN DEMAND FOR USER DEFINED INTERSECTIONS
     // user defined intersections won't have the necessary properties, so we use averages
     // since they're all the same no need to loop through, just multiply by the
     // number of user defined intersections
-    // existingTravel.miles.pedestrian.lower += avgIntersectionLower * userIntersections.length;
     existingTravel.miles.pedestrian.mean += avgIntersectionAvg * userIntersections.length;
-    // existingTravel.miles.pedestrian.upper += avgIntersectionUpper * userIntersections.length;
-
-    // existingTravel.capita.pedestrian.lower += (avgIntersectionLower * userIntersections.length) / avgIntersectionPops;
     existingTravel.capita.pedestrian.mean += (avgIntersectionAvg * userIntersections.length) / avgIntersectionPops;
-    // existingTravel.capita.pedestrian.upper += (avgIntersectionUpper * userIntersections.length) / avgIntersectionPops;
-
-    // existingTravel.jobs.pedestrian.lower += (avgIntersectionLower * userIntersections.length) / avgIntersectionJobs;
     existingTravel.jobs.pedestrian.mean += (avgIntersectionAvg * userIntersections.length) / avgIntersectionJobs;
-    // existingTravel.jobs.pedestrian.upper += (avgIntersectionUpper * userIntersections.length) / avgIntersectionJobs;
 
     // then the pedestrian demand is weighted by the project length and
     // number of intersections
@@ -132,45 +108,54 @@ const calcPedDemand = (existingTravel, selectedIntersections, userIntersections,
 
     if(numIntersections > 0) {
 
-      // existingTravel.miles.pedestrian.lower = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.miles.pedestrian.lower);
-      existingTravel.miles.pedestrian.mean = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.miles.pedestrian.mean);
-      // existingTravel.miles.pedestrian.upper = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.miles.pedestrian.upper);
+      console.log('ped')
 
-      // existingTravel.capita.pedestrian.lower = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.capita.pedestrian.lower);
-      existingTravel.capita.pedestrian.mean = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.capita.pedestrian.mean);
-      // existingTravel.capita.pedestrian.upper = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.capita.pedestrian.upper);
+      console.log(existingTravel.miles.pedestrian.mean);
 
-      // existingTravel.jobs.pedestrian.lower = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.jobs.pedestrian.lower);
-      existingTravel.jobs.pedestrian.mean = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.jobs.pedestrian.mean);
-      // existingTravel.jobs.pedestrian.upper = weightPedestrianDemand(projectLengthMiles, numIntersections, existingTravel.jobs.pedestrian.upper);
+      existingTravel.miles.pedestrian.mean = _weightDemand(
+        projectLengthMiles,
+        numIntersections,
+        existingTravel.miles.pedestrian.mean,
+        config.pedestrian
+      );
+
+      console.log(existingTravel.miles.pedestrian.mean);
+
+      existingTravel.capita.pedestrian.mean = _weightDemand(
+        projectLengthMiles,
+        numIntersections,
+        existingTravel.capita.pedestrian.mean,
+        config.pedestrian
+      );
+
+      existingTravel.jobs.pedestrian.mean = _weightDemand(
+        projectLengthMiles,
+        numIntersections,
+        existingTravel.jobs.pedestrian.mean,
+        config.pedestrian);
     }
 
     return existingTravel;
-
 }
 
-const calcBikeDemand = (existingTravel, selectedWays, userWays) => {
+const _calcBikeDemand = (
+  existingTravel,
+  selectedWays,
+  userWays,
+  projectLength) => {
 
-	// AVERAGE NEEDED PROPERTIES FOR USER SELECTED WAYS
+    // AVERAGE NEEDED PROPERTIES FOR USER SELECTED WAYS
     // Avg lower/mean/upper used for user defined ways
     // Avg pops/jobs used for user selected ways that are missing
     // these properties as well as user defined ways
-    // let wayLower = [];
     let wayAvg = [];
-    // let wayUpper = [];
     let wayPops = [];
     let wayJobs = [];
 
     for(let way of selectedWays) {
-      // if(way.properties.low_daily) {
-      //   wayLower.push(parseInt(way.properties.low_daily));
-      // }
       if(way.properties.bicyclist_demand) {
         wayAvg.push(parseInt(way.properties.bicyclist_demand));
       }
-      // if(way.properties.high_daily) {
-      //   wayUpper.push(parseInt(way.properties.high_daily));
-      // }
       if(way.properties.jobs) {
         wayJobs.push(way.properties.jobs);
       }
@@ -179,9 +164,7 @@ const calcBikeDemand = (existingTravel, selectedWays, userWays) => {
       }
     }
 
-    // let avgWayLower = wayLower.length ? wayLower.reduce((a,b) => a+b) / wayLower.length : null;
     let avgWayAvg = wayAvg.length ? wayAvg.reduce((a,b) => a+b) / wayAvg.length : null;
-    // let avgWayUpper = wayUpper.length ? wayUpper.reduce((a,b) => a+b) / wayUpper.length : null;
     let avgWayPop = wayPops.length ? wayPops.reduce((a,b) => a+b) / wayPops.length : null;
     let avgWayJobs = wayPops.length ? wayJobs.reduce((a,b) => a+b) / wayJobs.length : null;
 
@@ -197,9 +180,7 @@ const calcBikeDemand = (existingTravel, selectedWays, userWays) => {
         'jobs': {},
       };
 
-      // let lower = parseInt(way.properties.low_daily);
       let mean = parseInt(way.properties.bicyclist_demand);
-      // let upper = parseInt(way.properties.high_daily);
 
       // use properties for this way or the average of all selected ways if missing
       let population = way.properties.population ? way.properties.population : avgWayPop;
@@ -208,19 +189,13 @@ const calcBikeDemand = (existingTravel, selectedWays, userWays) => {
       // demand calcs all based on miles so convert feet -> miles here
       let wayLengthMiles = way.properties.length / 5280;
 
-      // current.miles.lower = lower * wayLengthMiles;
       current.miles.mean = mean * wayLengthMiles;
-      // current.miles.upper = upper * wayLengthMiles;
 
       // divide by population for per capita
-      // current.capita.lower = current.miles.lower / population;
       current.capita.mean = current.miles.mean / population;
-      // current.capita.upper = current.miles.upper / population;
 
       // divide by jobs for per jobs
-      // current.jobs.lower = current.miles.lower / jobs;
       current.jobs.mean = current.miles.mean / jobs;
-      // current.jobs.upper = current.miles.upper / jobs;
 
       waysTravel.push(current);
     }
@@ -239,83 +214,107 @@ const calcBikeDemand = (existingTravel, selectedWays, userWays) => {
 
       // use averages for everything here because user defined ways
       // won't have any of these properties
-      // current.miles.lower = avgWayLower * wayLengthMiles;
       current.miles.mean = avgWayAvg * wayLengthMiles;
-      // current.miles.upper = avgWayUpper * wayLengthMiles;
 
-      // current.capita.lower = current.miles.lower / avgWayPop;
       current.capita.mean = current.miles.mean / avgWayPop;
-      // current.capita.upper = current.miles.upper / avgWayPop;
 
-      // current.jobs.lower = current.miles.lower / avgWayJobs;
       current.jobs.mean = current.miles.mean / avgWayJobs;
-      // current.jobs.upper = current.miles.upper / avgWayJobs;
 
       waysTravel.push(current);
     }
 
     // grab total bike demand by summing all ways
     for(let travel of waysTravel) {
-      // existingTravel.miles.bike.lower += travel.miles.lower;
       existingTravel.miles.bike.mean += travel.miles.mean;
-      // existingTravel.miles.bike.upper += travel.miles.upper;
 
-      // existingTravel.capita.bike.lower += travel.capita.lower;
       existingTravel.capita.bike.mean += travel.capita.mean;
-      // existingTravel.capita.bike.upper += travel.capita.upper;
 
-      // existingTravel.jobs.bike.lower += travel.jobs.lower;
       existingTravel.jobs.bike.mean += travel.jobs.mean;
-      // existingTravel.jobs.bike.upper += travel.jobs.upper;
+    }
+
+    // then the bike demand is weighted by the project length and
+    // number of ways
+    let projectLengthMiles = projectLength / 5280;
+    let numWays = selectedWays.length + userWays.length;
+
+    if(numWays > 0) {
+
+      console.log('bike');
+
+      console.log(existingTravel.miles.bike.mean);
+
+      existingTravel.miles.bike.mean = _weightDemand(
+        projectLengthMiles,
+        numWays,
+        existingTravel.miles.bike.mean,
+        config.bike
+      );
+
+      console.log(existingTravel.miles.bike.mean);
+
+      existingTravel.capita.bike.mean = _weightDemand(
+        projectLengthMiles,
+        numWays,
+        existingTravel.capita.bike.mean,
+        config.bike
+      );
+
+      existingTravel.jobs.bike.mean = _weightDemand(
+        projectLengthMiles,
+        numWays,
+        existingTravel.jobs.bike.mean,
+        config.bike);
     }
 
     return existingTravel;
 
 }
 
-const calcDemand = (selectedWays, userWays, selectedIntersections, userIntersections, projectLength) => {
+const calcDemand = (
+  selectedWays,
+  userWays,
+  selectedIntersections,
+  userIntersections,
+  projectLength) => {
+
 	let existingTravel = {
       "miles": {
         "bike": {
-          // 'lower': 0,
           'mean': 0,
-          // 'upper': 0,
         },
         "pedestrian": {
-          // 'lower': 0,
           'mean': 0,
-          // 'upper': 0,
         }
       },
       "capita":  {
         "bike": {
-          // 'lower': 0,
           'mean': 0,
-          // 'upper': 0,
         },
         "pedestrian": {
-          // 'lower': 0,
           'mean': 0,
-          // 'upper': 0,
         }
       },
       "jobs":  {
         "bike": {
-          // 'lower': 0,
           'mean': 0,
-          // 'upper': 0,
         },
         "pedestrian": {
-          // 'lower': 0,
           'mean': 0,
-          // 'upper': 0,
         }
       },
     };
 
-    calcBikeDemand(existingTravel, selectedWays, userWays);
+    _calcBikeDemand(
+      existingTravel,
+      selectedWays,
+      userWays,
+      projectLength);
 
-    calcPedDemand(existingTravel, selectedIntersections, userIntersections, projectLength);
+    _calcPedDemand(
+      existingTravel,
+      selectedIntersections,
+      userIntersections,
+      projectLength);
 
     return existingTravel;
 }
